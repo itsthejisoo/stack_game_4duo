9f2e3fcf19833ff27cb15d125f740974a1272c46
Jisoo Kim <jisoo000528@naver.com>
2024-11-13 00:35:29 +0900

    GameOverActivity.java updated

--- Code/app/src/main/java/com/example/stack_forduo/GameOverActivity.java
+++ Code/app/src/main/java/com/example/stack_forduo/GameOverActivity.java
@@ -9,27 +9,39 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GameOverActivity extends AppCompatActivity {
-
+    private boolean isSingleMode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gameover);
+        isSingleMode = getIntent().getBooleanExtra("MODE", false);
+        if(!isSingleMode) {
+            setContentView(R.layout.activity_gameover_multi);
 
-        String winner = getIntent().getStringExtra("WINNER");
-        int player1Score = getIntent().getIntExtra("PLAYER1_SCORE", 0);
-        int player2Score = getIntent().getIntExtra("PLAYER2_SCORE", 0);
+            String winner = getIntent().getStringExtra("WINNER");
+            int player2Score = getIntent().getIntExtra("PLAYER2_SCORE", 0);
+            int player1Score = getIntent().getIntExtra("PLAYER1_SCORE", 0);
 
-        TextView winnerTextView = findViewById(R.id.winner);
-        winnerTextView.setText(winner);
+            TextView winnerTextView = findViewById(R.id.winner);
+            winnerTextView.setText(winner);
 
-        TextView player1ScoreView = findViewById(R.id.player1_score_view);
-        TextView player2ScoreView = findViewById(R.id.player2_score_view);
+            TextView player1ScoreView = findViewById(R.id.player1_score_view);
+            TextView player2ScoreView = findViewById(R.id.player2_score_view);
 
-        player1ScoreView.setText("Player 1 Score: " + player1Score);
-        player2ScoreView.setText("Player 2 Score: " + player2Score);
+            player1ScoreView.setText("Player 1 Score: " + player1Score);
+            player2ScoreView.setText("Player 2 Score: " + player2Score);
+        }
+        else {
+            setContentView(R.layout.activity_gameover_single);
 
-        Button playAgainButton = (Button) findViewById(R.id.playagain);
-        Button quitButton = (Button) findViewById(R.id.quitButton);
+            int player1Score = getIntent().getIntExtra("PLAYER1_SCORE", 0);
+
+            TextView player1ScoreView = findViewById(R.id.player1_score_view);
+
+            player1ScoreView.setText(player1Score);
+        }
+
+        Button playAgainButton = findViewById(R.id.playagain);
+        Button quitButton = findViewById(R.id.quitButton);
 
         // 스크린 전체화면 설정(appbar, homebar 숨기기)
         getWindow().getDecorView().setSystemUiVisibility(
@@ -44,6 +56,7 @@ public class GameOverActivity extends AppCompatActivity {
 
     public void onClickPlayAgain(View v) {
         Intent intent = new Intent(GameOverActivity.this, GameActivity.class);
+        intent.putExtra("MODE", isSingleMode ? "SINGLE" : "DOUBLE");
         startActivity(intent);
     }
 
